3c12f659987f083a8013e6012a583c49a76abb8c
nanakano <nakano.k.fit@gmail.com>
2020-07-20 14:11:26 +0900

    python3

--- Python3.x/main.py
+++ Python3.x/main.py
@@ -2,33 +2,52 @@
 
 # 1行コメント
 """ 複数行コメント """
-a   = 2
-b   = 5
-str1 = "Hello World!"
 
-print (str1)
-print ()
+def hello():
+  str1 = "Hello World!"
+  print (str1)
+  print ()
+  
+  return
 
-addition       = a + b
-subtraction    = a - b
-multiplication = a * b
-division       = a / b
+def add_sub_mult_dvi(a, b):
+  addition       = a + b
+  subtraction    = a - b
+  multiplication = a * b
+  division       = a / b
 
-print ("addition: ")
-print (a, "+", b, "=", addition)
-print ("subtraction: ")
-print (a, "-", b, "=", subtraction)
-print ("multiplication: ")
-print (a, "*", b, "=", multiplication)
-print ("division: ")
-print (a, "/", b, "=", division)
-print ()
+  print ("addition: ")
+  print (a, "+", b, "=", addition)
+  print ("subtraction: ")
+  print (a, "-", b, "=", subtraction)
+  print ("multiplication: ")
+  print (a, "*", b, "=", multiplication)
+  print ("division: ")
+  print (a, "/", b, "=", division)
+  print ()
 
-for i in range(4):
-  if i == 0:
-    print ("if for: ", i)
-  elif 0 < i and i < 3:
-    print ("elif for: ", i)
-  else:
-    print ("else for: ", i)
-print ()
+  return
+
+def fizz_buzz(count):
+  for i in range(1, count+1):
+    if i % 15 == 0:
+      print ("FizzBuzz! ", end='')
+    elif i % 5 == 0:
+      print ("Fizz! ")
+    elif i % 3 == 0:
+      print ("Buzz! ", end='')
+    else:
+      print (i, "", end='')
+
+    return
+
+def main():
+
+  hello()
+  add_sub_mult_dvi(2, 5)
+  fizz_buzz(100)
+
+  return
+
+if __name__ == "__main__":
+  main()
